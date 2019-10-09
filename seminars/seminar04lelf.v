bcbc15d363fb868d8a8a5e81068a21c1a5ab4f26
Antonio Nikishaev <me@lelf.lu>
2019-10-09 07:49:39 +0400

    sem4, +concise fib_add_succ

--- seminars/seminar04lelf.v
+++ seminars/seminar04lelf.v
@@ -6,6 +6,8 @@ Unset Printing Implicit Defensive.
 
 From Coq Require Import Omega Lia.
 From Hammer Require Import Hammer Reconstr.
+From QuickChick Require Import QuickChick Tactics.
+Set Bullet Behavior "None".
 
 
 Section InductionExercises.
@@ -38,23 +40,6 @@ Qed.
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 (** Write a tail-recursive variation of the [addn] function
     (let's call it [addn_iter]). *)
 Fixpoint add_iter (n m : nat) {struct n}: nat :=
@@ -74,26 +59,151 @@ Fixpoint fib (n : nat) : nat :=
   else n.
 Arguments fib n : simpl nomatch.
 
+
 Lemma leq_add1l p m n :
   m <= n -> m <= p + n.
 Proof.
-Search _ reflect.
+(* Hint Rewrite subn_eq0. *)
+(* Hint Resolve leq0n. *)
+(* Hint Rewrite leq_subLR. *)
 
+move/leP=> H. apply /leP. rewrite -plusE. lia.
+Restart.
+----Restart.
 by move/(leq_add (leq0n _)); apply.
+----Restart.
+by move/(leq_add (leq0n p)) ->.
 Qed.
+
+
+
 
 Lemma fib_monotone m n :
   m <= n -> fib m <= fib n.
-Proof.
+Proof with auto.
+
+elim: n. by case: m.
+elim.
+simpl.
+case: m... case...
+
+move=> /= n I' I.
+
+rewrite leq_eqVlt => /orP.
+case.
+move/eqP->. done.
+move/I. apply: leq_add1l.
+
+Restart.
+
+elim: n. by case: m.
+elim.
+simpl.
+case: m => //; case => //.
+
+rewrite/fib. 
+
+move=> /= n I' I.
+
+rewrite leq_eqVlt => /orP.
+case.
+move/eqP->. done.
+move/I. apply: leq_add1l.
+
+
+(* Restart. *)
+
+(* elim: n m=> [|n I] m. by case: m. *)
+(* elim. *)
+(* simpl. case: m. done. *)
+
+(* move=> /= n'.  *)
+(* rewrite leq_eqVlt. => /orP. *)
+(* case. *)
+(* move/eqP->. done. *)
+
+
+
+Restart.
+
+
+
+------Restart.
+
 elim: n=> [|[|n] IHn]; first by case: m.
 - by case: {IHn}m=> [|[]].
 rewrite /= leq_eqVlt => /orP[/eqP->// | /IHn].
 by apply: leq_add1l.
 Qed.
 
+
+
+
+
+
+Goal forall n, fib (0 + n.+1).+1 = fib 1 * fib n.+2 + fib 0 * fib n.+1.
+cbn. move=> n.
+autorewrite with yhints.
+trivial.
+Qed.
+
+
 Lemma fib_add_succ m n :
   fib (m + n).+1 = fib m.+1 * fib n.+1 + fib m * fib n.
 Proof.
+
+
+  (* 
+
+
+  IHm : forall n : nat,
+        fib (m + n).+1 = fib m.+1 * fib n.+1 + fib m * fib n
+  n : nat
+  ============================
+  fib (m + n.+1)%Nrec + fib (m.+1 + n.+1) =
+  (fib m + fib m.+1) * (fib n + fib n.+1) + fib m.+1 * fib n.+1
+
+A m : nat
+  IHm : forall n : nat,
+        fib (m + n).+1 = fib m.+1 * fib n.+1 + fib m * fib n
+  ============================
+  forall n : nat,
+  fib (m + n.+1) + (fib m.+1 * (fib n + fib n.+1) + fib m * fib n.+1) =
+  (fib m + fib m.+1) * (fib n + fib n.+1) + fib m.+1 * fib n.+1
+
+  m : nat
+  IHm : forall n : nat,
+        fib (m + n).+1 = fib m.+1 * fib n.+1 + fib m * fib n
+  n : nat
+  ============================
+  fib (m.+1 + n.+1).+1 = fib m.+2 * fib n.+2 + fib m.+1 * fib n.+1
+
+
+
+ *)
+
+elim: m n=> [|m IHm] [//|n]; try by rewrite ?muln0 !addn0 ?mul1n ?muln1.
+rewrite addSn /= IHm addnS IHm /= -!plusE -!multE; lia.
+
+
+--------Restart.
+elim: m n=> [|m IHm] [//|n] .
+- by rewrite addn0 mul1n. 
+- by rewrite muln0 !addn0 muln1.
+rewrite addSn /= IHm addnS IHm /=.
+rewrite -!plusE -!multE; lia.
+
+
+--------Restart.
+elim: m n=> [|m IHm] [|n] //. 
+- by rewrite addn0 mul1n. 
+- by rewrite muln0 !addn0 muln1.
+
+rewrite addSn /= IHm addnS IHm /=.
+rewrite -!plusE -!multE; lia.
+
+
+--------Restart.
 elim: m n=> [|m IHm] n; first by rewrite mul1n addn0.
 rewrite addSn /= IHm.
 case: n=> [|n].
@@ -102,8 +212,8 @@ rewrite addnS {}IHm /= mulnDl !mulnDr !addnA.
 rewrite -!plusE; omega.
 Qed.
 
-End InductionExercises.
 
+End InductionExercises.
 
 
 (* Thanks to Mike Potanin for pointing me to this example *)
@@ -124,7 +234,13 @@ Hypothesis I : interchange f1 f2.
 
 Lemma units_same :
   e1 = e2.
-Proof. by move: (I e1 e2 e2 e1); do ! rewrite ?U1 ?U2. Qed.
+Proof.
+move: I. clear I. rewrite/interchange => I.
+move: U1 U2. clear U1 U2. rewrite/left_id/right_id => U1 U2.
+move: (I e1 e2 e2 e1).
+rewrite ?U1 ?U2 ?U1.
+done.
+Qed.
 
 Lemma operations_equal :
   f1 =2 f2.
